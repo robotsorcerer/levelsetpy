6c8c1a52bff9151ba503dcea47199272c36939ed
robotsorcerer <patlekano@gmail.com>
2025-03-31 23:09:30 -0400

    can't make it to cdc this year; neurips maybe

--- sample-reach/viscous/gmm.py
+++ sample-reach/viscous/gmm.py
@@ -1,4 +1,3 @@
-""" This file defines a Gaussian mixture model class. """
 import logging
 
 import torch 
@@ -30,18 +29,23 @@ def check_sigma(A):
         return Anew
 
 class GMM:
-    """ Gaussian Mixture Model. """
-    def __init__(self, init_sequential=False, eigreg=False, warmstart=True):
+    """ 
+        Expecttaion-maximization operator for the normal inverse Wishart surrogate used to compute the 
+        proximal operator for the spatial graidents of the value function.
+    """
+
+    def __init__(self, device='cpu', init_sequential=False, eigreg=False, warmstart=True):
         self.init_sequential = init_sequential
         self.eigreg = eigreg
         self.warmstart = warmstart
         self.sigma = None
+        self.device = device
         
     def inference(self, pts):
         """
-        Evaluate dynamics prior.
-        Args:
-            pts: A N x D tensor of points.
+            Evaluate dynamics prior.
+            Args:
+                pts: A N x D tensor of points.
         """
         logwts = self.clusterwts(pts)
         mu0, Phi = self.moments(logwts)
@@ -49,24 +53,30 @@ class GMM:
         n0 = m - 2 - mu0.shape[0]
         m = float(m) / self.N
         n0 = float(n0) / self.N
+
         return mu0, Phi, m, n0
 
     def clusterwts(self, data):
         """
-        Compute cluster weights for specified points under GMM.
+            Compute cluster weights for specified points under the Gaussian distribution.
         """
+
         logobs = self.estep(data)
         logwts = logobs - torch.logsumexp(logobs, dim=1, keepdim=True)
         logwts = torch.logsumexp(logwts, dim=0) - torch.log(torch.tensor(data.shape[0]))
+
         return logwts.T
 
     def estep(self, data):
         """
-        Compute log observation probabilities under GMM.
+            Compute log observation probabilities under expectation maximization.
         """
-        N, D = data.shape
+
+        N, D = data.shape[:2]
         K = self.sigma.shape[0]
-        logobs = -0.5 * torch.ones((N, K), device=data.device) * D * torch.log(torch.tensor(2 * torch.pi))
+        logobs = -0.5 * torch.ones_like((N, K), device=self.device) * D * torch.log(torch.tensor(2 * torch.pi))
+        # logobs = -0.5 * torch.ones_like(data, device=self.device) * D * torch.log(torch.tensor(2 * torch.pi))
+
         for i in range(K):
             mu, sigma = self.mu[i], self.sigma[i]
             L = torch.linalg.cholesky(sigma)
@@ -74,12 +84,14 @@ class GMM:
             diff = (data - mu).T
             soln = torch.linalg.solve_triangular(L, diff, upper=False)
             logobs[:, i] -= 0.5 * torch.sum(soln ** 2, dim=0)
+
         logobs += self.logmass.T
+
         return logobs
 
     def moments(self, logwts):
         """
-        Compute the moments of the cluster mixture with logwts.
+        Compute the moments of the cluster with logwts.
         """
         wts = torch.exp(logwts)
         mu = torch.sum(self.mu * wts, dim=0)
@@ -87,4 +99,101 @@ class GMM:
         diff_expand = self.mu.unsqueeze(1) * diff.unsqueeze(2)
         wts_expand = wts.unsqueeze(2)
         sigma = torch.sum((self.sigma + diff_expand) * wts_expand, dim=0)
+
         return mu, sigma
+    
+    def update(self, data, K, max_iterations=100):
+        """
+        Run EM to update clusters.
+        Args:
+            data: An N x D data matrix, where N = number of data points.
+            K: Number of clusters to use.
+        """
+        # Constants.
+        N = data.shape[0]
+        Do = data.shape[1]
+
+        LOGGER.debug('Fitting GMM with %d clusters on %d points.', K, N)
+
+        if (not self.warmstart or self.sigma is None or
+                K != self.sigma.shape[0]):
+            # Initialization.
+            LOGGER.debug('Initializing GMM.')
+            self.sigma = torch.zeros((K, Do, Do), device=data.device)
+            self.mu = torch.zeros((K, Do), device=data.device)
+            self.logmass = torch.log(torch.ones(K, 1, device=data.device) / K)
+            self.mass = torch.ones(K, 1, device=data.device) / K
+            self.N = data.shape[0]
+            N = self.N
+
+            # Set initial cluster indices.
+            if not self.init_sequential:
+                cidx = torch.randint(0, K, (1, N), device=data.device)
+            else:
+                raise NotImplementedError()
+
+            # Initialize.
+            for i in range(K):
+                cluster_idx = (cidx == i).squeeze()
+                mu = torch.mean(data[cluster_idx, :], dim=0)
+                diff = (data[cluster_idx, :] - mu).T
+                sigma = (1.0 / K) * torch.matmul(diff, diff.T)
+                self.mu[i, :] = mu
+                self.sigma[i, :, :] = sigma + torch.eye(Do, device=data.device) * 2e-6
+
+        prevll = -float('inf')
+        for itr in range(max_iterations):
+            # E-step: compute cluster probabilities.
+            logobs = self.estep(data)
+
+            # Compute log-likelihood.
+            ll = torch.sum(torch.logsumexp(logobs, dim=1))
+            LOGGER.debug('GMM itr %d/%d. Log likelihood: %f',
+                         itr, max_iterations, ll)
+            if ll < prevll:
+                LOGGER.debug('Log-likelihood decreased! Ending on itr=%d/%d',
+                             itr, max_iterations)
+                break
+            if torch.abs(ll - prevll) < 1e-5 * prevll:
+                LOGGER.debug('GMM converged on itr=%d/%d',
+                             itr, max_iterations)
+                break
+            prevll = ll
+
+            # Renormalize to get cluster weights.
+            logw = logobs - torch.logsumexp(logobs, dim=1, keepdim=True)
+            assert logw.shape == (N, K)
+
+            # Renormalize again to get weights for refitting clusters.
+            logwn = logw - torch.logsumexp(logw, dim=0, keepdim=True)
+            assert logwn.shape == (N, K)
+            w = torch.exp(logwn)
+
+            # M-step: update clusters.
+            # Fit cluster mass.
+            self.logmass = torch.sum(logw, dim=0).view(-1, 1)
+            self.logmass = self.logmass - torch.logsumexp(self.logmass, dim=0, keepdim=True)
+            assert self.logmass.shape == (K, 1)
+            self.mass = torch.exp(self.logmass)
+
+            # Reboot small clusters.
+            w[:, (self.mass < (1.0 / K) * 1e-4).squeeze()] = 1.0 / N
+            # Fit cluster means.
+            w_expand = w.unsqueeze(2)
+            data_expand = data.unsqueeze(1)
+            self.mu = torch.sum(w_expand * data_expand, dim=0)
+            # Fit covariances.
+            wdata = data_expand * torch.sqrt(w_expand)
+            assert wdata.shape == (N, K, Do)
+            for i in range(K):
+                # Compute weighted outer product.
+                XX = torch.matmul(wdata[:, i, :].T, wdata[:, i, :])
+                mu = self.mu[i, :]
+                self.sigma[i, :, :] = XX - torch.outer(mu, mu)
+
+                if self.eigreg:  # Use eigenvalue regularization.
+                    raise NotImplementedError()
+                else:  # Use quick and dirty regularization.
+                    sigma = self.sigma[i, :, :]
+                    self.sigma[i, :, :] = 0.5 * (sigma + sigma.T) + \
+                            1e-6 * torch.eye(Do, device=data.device)
